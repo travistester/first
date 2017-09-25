ebc2f50038c39c5869213859dc08512ab051a53c
travistester <travistester@example.com>
2017-09-25 19:15:21 +0530

    make function

--- .travis/install.py
+++ .travis/install.py
@@ -2,5 +2,5 @@ import sys
 print(sys.version)
 
 from time import time
-print(time)
+print(time())
 print ('bawaseer install\n'*10)
--- .travis/run.py
+++ .travis/run.py
@@ -2,5 +2,5 @@ import sys
 print(sys.version)
 
 from time import time
-print(time)
+print(time())
 print ('bawaseer run\n'*10)
